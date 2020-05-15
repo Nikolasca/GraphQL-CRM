f93d0aacca9038c135490f5639ea2cd5e0745940
Níkolas <nikolas_cardona@hotmail.com>
2020-05-14 23:20:33 -0500

    crm

--- db/schema.js
+++ db/schema.js
@@ -4,17 +4,10 @@ const {  gql } = require('apollo-server');
 //Schema
 const typeDefs = gql`
 
-    type Curso{
-        titulo: String     
-    }
-    type Tecno{
-        tecnologia: String
-    }
-    type Query{
-        obtenerCursos: Curso
-        obtenerTecno: [Tecno]
-    }
+        type Query{
+            obtenerCurso: String
 
+        }
     `;
 
     module.exports = typeDefs;
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const {ApolloServer} = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
+const conectarDB = require('./config/db');
+
+
+//Iniciar conexión BD
+conectarDB();
 
 //servidor
 const server = new ApolloServer({
